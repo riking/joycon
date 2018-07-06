144b22de6c286e0a17fea23be5ed4321cb90c987
Andrew Nickell <nickell.andrew@gmail.com>
2018-07-05 22:12:28 -0700

    add remap arg to output_generic

--- prog4/jcdriver/output_generic.go
+++ prog4/jcdriver/output_generic.go
@@ -8,7 +8,7 @@ import (
 )
 
 func getOutputFactory() jcpc.OutputFactory {
-	return func(t jcpc.JoyConType, playerNum int) (jcpc.Output, error) {
+	return func(t jcpc.JoyConType, playerNum int, remap InputRemappingOptions) (jcpc.Output, error) {
 		return output.NewConsole(t, playerNum)
 	}
 }
